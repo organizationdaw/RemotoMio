043f20e13192d9a3b364d2e9d289713aefcaf0b7
blopez68 <blopez@salesianasnspilar.org>
2016-05-08 08:53:58 +0200

    Quito comentario

--- Prueba/src/prueba/Main.java
+++ Prueba/src/prueba/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
 	/* He añadido un comentario*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Prueba");
+			System.out.println("Prueba");
 
 	}
 
