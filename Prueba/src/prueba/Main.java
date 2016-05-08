f872ba36c8f2c6faf74a7fd7f7497110d42044a8
blopez68 <blopez@salesianasnspilar.org>
2016-05-08 08:40:12 +0200

    Segundo cambio

--- Prueba/src/prueba/Main.java
+++ Prueba/src/prueba/Main.java
@@ -2,6 +2,7 @@ package prueba;
 
 public class Main {
 
+	/* He añadido un comentario*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba");
